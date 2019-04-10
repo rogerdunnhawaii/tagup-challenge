e0babe1c54619c9c1b4af1f3877e612a38c4f49b
rogerdunnGA <rogerdunn@Gmail.com>
2019-04-10 16:58:21 -0400

    Updates

---
+++ precache-manifest.9ba61b09e3f922a7bb3534d4ea9bbb1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f24ec530ec4abef92c6",
+    "url": "/tagup-challenge/static/js/runtime~main.e7dde2d0.js"
+  },
+  {
+    "revision": "683b93c587993d6045c4",
+    "url": "/tagup-challenge/static/js/main.6c37e599.chunk.js"
+  },
+  {
+    "revision": "0fad6e4e46f8df8ca708",
+    "url": "/tagup-challenge/static/js/2.02c03233.chunk.js"
+  },
+  {
+    "revision": "683b93c587993d6045c4",
+    "url": "/tagup-challenge/static/css/main.5a9ef3a3.chunk.css"
+  },
+  {
+    "revision": "f66b3601da28396c5d4918828e41b054",
+    "url": "/tagup-challenge/index.html"
+  }
+];
